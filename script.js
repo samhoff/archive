5b0e66ab0bcfd2890ec228d4d25f11cb67ddaa24
samhoff <samhoff@ucla.edu>
2015-12-09 00:23:32 -0800

    Uploading files

---
+++ script.js
@@ -0,0 +1,5 @@
+﻿function update() {
+    var archivevalues = document.getElementById("date").value.split(":");
+    document.getElementById("viewer").innerHTML = '<iframe src="https://archive.org/stream/ucladailybruin' + archivevalues[0] + 'losa?ui=zoom#page/n' + archivevalues[1] + '/mode/1up" width="100%" height="100%" frameborder="0"></iframe>'
+        ;
+}
